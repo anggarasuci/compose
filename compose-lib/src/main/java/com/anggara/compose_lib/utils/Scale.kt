1187ff3ccd5d3e98935bf4997c85b0d2a94ccb9e
Anggara Suci Nugraha <anggarasucinugraha@gmail.com>
2024-07-05 00:00:47 +0700

    implement scale factor enum

--- compose-lib/src/main/java/com/anggara/compose_lib/utils/Scale.kt
+++ compose-lib/src/main/java/com/anggara/compose_lib/utils/Scale.kt
@@ -4,18 +4,23 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.Dp
 
+enum class ScaleFactor(val value: Float) {
+    X_LARGE(1.3f),
+    LARGE(1.15f),
+    NORMAL(1.0f)
+}
+
 @Composable
 private fun getScaleFactor(): Float {
     val configuration = LocalConfiguration.current
     val screenWidthDp = configuration.screenWidthDp
 
     return when {
-        screenWidthDp > 800 -> 1.3f // xLarge
-        screenWidthDp > 600 -> 1.15f // Large
-        else -> 1.0f // Normal
+        screenWidthDp > 800 -> ScaleFactor.X_LARGE.value
+        screenWidthDp > 600 -> ScaleFactor.LARGE.value
+        else -> ScaleFactor.NORMAL.value
     }
 }
-
 
 @Composable
 fun Float.scaledSize(): Float {
