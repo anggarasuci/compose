880247b906530c4fb8e62e8fe0ce974225e3caf2
Anggara Suci Nugraha <anggarasucinugraha@gmail.com>
2024-07-26 17:35:16 +0700

    adjust: add elevation card

--- compose-lib/src/main/java/com/anggara/compose_lib/ui/card/Card.kt
+++ compose-lib/src/main/java/com/anggara/compose_lib/ui/card/Card.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -23,6 +24,7 @@ fun CardContainer(
     contentPadding: PaddingValues = PaddingValues(space.x4),
     backgroundColor: Color = Color.Neutral10,
     borderColor: Color? = null,
+    elevation: CardElevation = CardDefaults.cardElevation(),
     content: @Composable () -> Unit
 ) {
     val colors = CardDefaults.cardColors().copy(
@@ -36,7 +38,8 @@ fun CardContainer(
         modifier = modifier,
         colors = colors,
         border = borderStroke,
-        shape = RoundedCornerShape(radius)
+        shape = RoundedCornerShape(radius),
+        elevation = elevation
     ) {
         Column(
             modifier = Modifier
